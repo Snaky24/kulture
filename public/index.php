3698ceb1547b34d7fa108826c87c1494ba1ed51b
Camille <kultureasian@gmail.com>
2024-12-17 14:38:45 -0500

    Premier

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php $niveau = "./"; ?>
+<?php include($niveau . "liaisons/php/config.inc.php"); ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="">
+	<meta name="keyword" content="">
+	<meta name="author" content="">
+	<meta charset="utf-8">
+	<link class="favicon" rel="icon" type="image/x-icon" href="liaisons/images/svg/favicon.svg">
+	<title>Accueil | GO-ALL</title>
+	<?php include($niveau . "liaisons/fragments/headlinks.inc.php"); ?>
+</head>
+
+<body class="body__accueil">
+
+	<?php include($niveau . "liaisons/fragments/entete.inc.php"); ?>
+
+
+	<?php include($niveau . "liaisons/fragments/piedDePage.inc.php"); ?>
